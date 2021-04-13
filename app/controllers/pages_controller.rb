409a7704a838be5b9f1544d4e0d6b2724fb91472
Nebojša Stričević <strika@hackberry.dev>
2021-04-13 05:09:57 +0000

    Use Faker to generate recipes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,5 @@
 class PagesController < ApplicationController
   def main
-    @recipes = [
-      Recipe.new("Air Fried Falafel", "vegan, simple", "..."),
-      Recipe.new("Deviled Chicken Breasts", "quick and easy", "..."),
-      Recipe.new("Alfredo Sauce", "rich an creamy", "..."),
-      Recipe.new("Waffles", "basic")
-    ]
+    @recipes = Array.new(6, Recipe.new)
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,1 +1,9 @@
-Recipe = Struct.new(:title, :subtitle, :description)
+class Recipe
+  attr_reader :title, :subtitle, :description
+
+  def initialize
+    @title       = Faker::Food.dish
+    @subtitle    = "#{Faker::Food.ingredient}, #{Faker::Food.spice}"
+    @description = Faker::Food.description
+  end
+end
