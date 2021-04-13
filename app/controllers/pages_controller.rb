34fc92ae62fdecc2357b51d61d2c40511ea9c6d9
Nebojša Stričević <strika@hackberry.dev>
2021-04-13 04:55:49 +0000

    Show recipes on main page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,10 @@
 class PagesController < ApplicationController
-  def main; end
+  def main
+    @recipes = [
+      Recipe.new("Air Fried Falafel", "vegan, simple", "..."),
+      Recipe.new("Deviled Chicken Breasts", "quick and easy", "..."),
+      Recipe.new("Alfredo Sauce", "rich an creamy", "..."),
+      Recipe.new("Waffles", "basic")
+    ]
+  end
 end
---
+++ app/models/recipe.rb
@@ -0,0 +1,1 @@
+Recipe = Struct.new(:title, :subtitle, :description)
